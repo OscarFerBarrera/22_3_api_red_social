fcc7b7febef3b25175c42f8d78eb4c887132c9e3
Oscar Barrera <osjamfer@hotmail.com>
2024-05-12 23:21:43 +0200

    add model repair ts

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 22_3_api_red_social
+
+Entidades:
+  user
+    nombre
+    apellido
+    mail
+    password
+    birthdate
+    gender
+
+  frend
+    nombre
+    mail
+    birthdate
+    gender
+
+ignorePatterns: ["dist/*", ".eslintrc.js"],
---
+++ src/models/User.ts
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  birthDate: Date;
+  gender: string;
+}
+
+// Creamos el schema del usuario
+const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 45,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 45,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (text: string) => validator.isEmail(text),
+        message: "Email incorrecto",
+      },
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: [8, "La contraseña debe tener al menos 8 caracteres"],
+      select: false,
+    },
+    birthDate: {
+      type: Date,
+      required: false,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    // Si la contraseña ya estaba encriptada, no la encriptamos de nuevo
+    if (this.isModified("password")) {
+      const saltRounds = 10;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const passwordEncrypted = await bcrypt.hash(this.password, saltRounds);
+      this.password = passwordEncrypted;
+    }
+
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+export const User = mongoose.model<IUser>("User", userSchema);
